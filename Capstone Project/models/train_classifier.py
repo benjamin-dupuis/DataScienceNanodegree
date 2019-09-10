c60722f25e41ea397a5cfb83322e6e42b21e72b7
Benjamin <bdupuis@coveo.com>
2019-09-09 21:10:26 -0400

    Improve machine learning model.

--- Capstone Project/models/train_classifier.py
+++ Capstone Project/models/train_classifier.py
@@ -3,7 +3,8 @@ import sys
 import time
 
 import pandas as pd
-from sklearn.ensemble import GradientBoostingClassifier
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split, GridSearchCV
 
@@ -12,7 +13,7 @@ def load_data(data_path):
     """Load DataFrame previously engineered for machine learning. """
     df = pd.read_csv(data_path)
     X = df.drop('successful_offer', axis=1)
-    y = df['successful_offer']
+    y = df['successful_offer'].astype(int)
     return X, y
 
 
@@ -39,15 +40,16 @@ def train_and_test_model(model, X_train, X_test, y_train, y_test, print_time=Tru
 def build_model(random_state=42, cross_validation_folds=3):
     """ Build an GradientBoostingClassifier model. """
 
-    gbc = GradientBoostingClassifier(random_state=random_state)
+    rf = RandomForestClassifier(random_state=random_state, n_estimators=100)
 
-    parameters = {'loss': ['deviance', 'exponential'],
-                  'learning_rate': [0.1, 0.2, 0.3],
-                  'n_estimators': [20, 50, 100, 150],
-                  'max_depth': [3, 4, 5],
-                  'min_samples_split': [2, 3, 4]}
+    parameters = {'n_estimators': [50, 70, 100, 200],
+                  'min_samples_split': [3, 9, 15],
+                  'min_samples_leaf': [1, 3, 5],
+                  'max_depth': [None, 1, 2, 3, 5],
+                  'random_state': [random_state],
+                  }
 
-    grid_object = GridSearchCV(gbc, parameters, cv=cross_validation_folds)
+    grid_object = GridSearchCV(rf, parameters, cv=cross_validation_folds)
 
     return grid_object
 
@@ -64,7 +66,11 @@ def main():
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    FILEPATH: {}'.format(database_filepath))
         X, y = load_data(database_filepath)
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+        # Over sampling to have the same amout of datapoints of both labels.
+        ros = RandomOverSampler()
+        X_ros, y_ros = ros.fit_sample(X, y)
+        X_train, X_test, y_train, y_test = train_test_split(X_ros, y_ros, test_size=0.2)
 
         print('Building model...')
         model = build_model()
